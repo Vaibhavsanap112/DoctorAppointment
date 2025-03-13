8dfade16c24132667de089697a9abb58324fcd79
Vaibhav <sanapvaibhav8767@gmail.com>
2025-03-13 11:22:55 +0530

    frontend is done

--- Frontend/src/pages/Doctors.jsx
+++ Frontend/src/pages/Doctors.jsx
@@ -5,6 +5,7 @@ import { AppContext } from "../contest/AppContext";
 function Doctors() {
   const { speciality } = useParams();
   const [filterDoc, setFileterDoc] = useState([]);
+  const [showFilter,setShowFilter] = useState(false)
   const navigate = useNavigate();
   const { doctors } = useContext(AppContext);
   const applyFilter = () => {
@@ -22,7 +23,14 @@ function Doctors() {
     <div>
       <p className="text-gray-600">Browse through the doctors specialist.</p>
       <div className="flex  flex-col sm:flex-row items-start gap-5 mt-5">
-        <div className="flex flex-col gap-4 text-sm text-gray-600">
+      <button 
+  className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter ? 'bg-[#5f6fff]' : ''}`}
+  onClick={() => setShowFilter(prev => !prev)}
+>
+  Filters
+</button>
+
+        <div className={` flex-col gap-4 text-sm text-gray-600 ${showFilter ? 'flex':'hidden sm:flex'}`}>
           <p
             onClick={() =>
               speciality === "General physician"
